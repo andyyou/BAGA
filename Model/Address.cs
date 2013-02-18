3c302f2cdb11345f1dc29436c5e4fbc5156281c4
AndyYou <andyyu0920@gmail.com>
2013-02-18 18:01:16 +0800

    use AddressId and set complex type

--- Model/Address.cs
+++ Model/Address.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [ComplexType]
     public class Address
     {
         // Use Complex type
-        // public int AddressId { get; set; }
+        public int AddressId { get; set; }
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
