bd614803b0bcc32a30ac483391632bef59c8a437
AndyYou <andyyu0920@gmail.com>
2013-02-18 12:02:39 +0800

    fixed main

--- BreakAwayConsole/Program.cs
+++ BreakAwayConsole/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
-using DataAccess;
+using DataAccessForAnnotation;
 using System.Data.Entity;
 
 namespace BreakAwayConsole
