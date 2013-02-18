bd58fb9f43e2ec09ca4c5a4356026c5d1ab50ba5
AndyYou <andyyu0920@gmail.com>
2013-02-18 13:21:29 +0800

    add model of trip and set key

--- DataAccess/BreakAwayContext.cs
+++ DataAccess/BreakAwayContext.cs
@@ -12,5 +12,6 @@ namespace DataAccessForAnnotation
     {
         public DbSet<Destination> Destinations { get; set; }
         public DbSet<Lodging> Lodgings { get; set; }
+        public DbSet<Trip> Trips { get; set; }
     }
 }
---
+++ Model/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Trip
+    {
+        [Key]
+        public Guid Identifier { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal CostUSD { get; set; }
+    }
+}
