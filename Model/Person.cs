7e18523a9b8d82aad7a0521506fed686ca13a455
AndyYou <andyyu0920@gmail.com>
2013-02-18 17:50:07 +0800

    add complex type model of Address into Person class

--- Model/Person.cs
+++ Model/Person.cs
@@ -10,6 +10,7 @@ namespace Model
 {
     public class Person
     {
+
         public int PersonId { get; set; }
         // [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         [ConcurrencyCheck] // 更新，刪除，都會多確認SocialSecurityNumber是否一致。
@@ -18,5 +19,11 @@ namespace Model
         public string LastName { get; set; }
         [Timestamp]
         public byte[] RowVersion { get; set; }
+        public Address Address { get; set; }
+
+        public Person()
+        {
+            Address = new Address();
+        }
     }
 }
