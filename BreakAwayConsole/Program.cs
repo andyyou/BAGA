a91cdbfe2d5ed10ce0e26e2db0ed6aec400fd2df
AndyYou <andyyu0920@gmail.com>
2013-02-18 13:56:58 +0800

    add InsertTirp() and check Guid is not auto generate

--- BreakAwayConsole/Program.cs
+++ BreakAwayConsole/Program.cs
@@ -15,6 +15,7 @@ namespace BreakAwayConsole
         {
             Database.SetInitializer(new DropCreateDatabaseIfModelChanges<BreakAwayContext>());
             InsertDestination();
+            InsertTrip();
         }
 
         private static void InsertDestination()
@@ -29,6 +30,21 @@ namespace BreakAwayConsole
             {
                 context.Destinations.Add(destination);
                 context.SaveChanges();
+            }
+        }
+
+        private static void InsertTrip()
+        {
+            var trip = new Trip { 
+                CostUSD = 800,
+                StartDate = new DateTime(2009,9,21),
+                EndDate = new DateTime(2012,12,21)
+            };
+
+            using (var context = new BreakAwayContext())
+            {
+                context.Trips.Add(trip);
+                context.SaveChanges();   
             }
         }
     }
