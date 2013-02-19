8cad70af05199cb606d0a18bc627a8982686adc8
AndyYou <andyyu0920@gmail.com>
2013-02-19 13:32:10 +0800

    configuring multiple bidirectional relationship from Lodging to Person

--- Model/Lodging.cs
+++ Model/Lodging.cs
@@ -23,5 +23,9 @@ namespace Model
         public Destination Destination { get; set; }
 
         public List<InternetSpecial> InternetSpecials { get; set; }
+        [InverseProperty("PrimaryContactFor")]
+        public Person PrimaryContact { get; set; }
+        [InverseProperty("SecondaryContactFor")]
+        public Person SecondaryContact { get; set; }
     }
 }
