86789206162fbad221724ca6a92df2adf3171e94
AndyYou <andyyu0920@gmail.com>
2013-02-19 10:18:27 +0800

    realize about default setting of FK

--- Model/Lodging.cs
+++ Model/Lodging.cs
@@ -17,7 +17,8 @@ namespace Model
         public string Name { get; set; }
         public string Owner { get; set; }
         public bool IsResort { get; set; }
-        [Required]
-        public Destination Destination { get; set; }
+        // [Required]
+        // public Destination Destination { get; set; }
+        // public int DestinationId { get; set; }
     }
 }
